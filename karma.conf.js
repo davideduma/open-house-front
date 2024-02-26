9d33c4f3d8594fca5819ee16b3f5f04370efa24a
davideduma <dmarquez@bancolombia.com.co>
2024-02-26 14:51:08 -0500

    Updated

--- karma.conf.js
+++ karma.conf.js
@@ -40,7 +40,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadlessNoSandbox'], //ChromeHeadless
+    browsers: ['Chrome'], //ChromeHeadless
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
